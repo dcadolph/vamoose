6f659678b322d0e2a95495b2d88f6890688c8b6e
Douglas Adolph <d.adolph.atx@gmail.com>
2026-07-12 20:52:29 -0500

    Set version to 0.29.0

--- cmd/version.go
+++ cmd/version.go
@@ -6,7 +6,7 @@ import (
 )
 
 // version is the semantic version, overridable at build time via -ldflags.
-var version = "0.28.0"
+var version = "0.29.0"
 
 // versionString returns the version, appending the VCS revision when available.
 func versionString() string {
